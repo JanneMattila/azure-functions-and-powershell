ae74c815b7ac97e4b56672321cbe400adad342b8
Janne Mattila <janne.mattila@hotmail.fi>
2023-10-21 15:52:18 +0300

    Add simple scanning implementation

--- src/MaintenanceTasks/Scripts/ScanVirtualMachines.ps1
+++ src/MaintenanceTasks/Scripts/ScanVirtualMachines.ps1
@@ -1,19 +1,71 @@
 <#
 Example invocation:
-$result = .\Scripts\ScanVirtualMachines.ps1 -Param1 "Hello" -Param2 123
-$result.Parameter1
-$result.Parameter2
+$result = .\Scripts\ScanVirtualMachines.ps1 -TagName "schedule" -Count 1000
+$result.VirtualMachines
+$result.Continues
 #>
 param (
-    [string]$Param1,
-    [int]$Param2
+    [string]$TagName = "schedule",
+    [string]$PowerState = "PowerState/running",
+    [int]$Count = 10
 )
 
-# Create a PowerShell object
-$result = [PSCustomObject]@{
-    Parameter1 = $Param1
-    Parameter2 = $Param2
+class VirtualMachineData {
+    [string] $Name
+    [string] $Subscription
+    [string] $ResourceGroup
+    [string] $Location
+    [string] $Schedule
 }
 
-# Return the PowerShell object
-return $result
+$query = @"
+resources 
+| where type =~ 'Microsoft.Compute/virtualMachines'
+| where isnotnull(tags['$TagName']) 
+| extend schedule = tags['$TagName']
+| extend PowerState = tostring(properties.extended.instanceView.powerState.code)
+| where PowerState == '$PowerState'
+| project subscriptionId, resourceGroup, name, schedule, location
+| project-rename  Subscription = subscriptionId, ResourceGroup = resourceGroup, Name = name, Schedule = schedule, Location = location
+| order by Subscription, ResourceGroup, Name
+"@
+Write-Host $query
+
+$results = Search-AzGraph -Query $query -First $Count
+$results | Format-Table
+
+$virtualMachines = New-Object System.Collections.ArrayList
+
+foreach ($result in $results) {
+
+    # Schedule is defined in "start-end" 24 hour format. Example: "8-16".
+    if ($result.Schedule.Contains("-") -eq $false) {
+        Write-Warning "Schedule tags value '$($result.Schedule)' for virtual machine $($result.Name) is not in correct 'start-end' in 24 hour format."
+        continue
+    }
+
+    # Let's verify if that schedule is active now.
+    $start = [int]$result.Schedule.Split("-")[0]
+    $end = [int]$result.Schedule.Split("-")[1]
+    $now = [int](Get-Date -Format "HH" -AsUTC)
+    if ($now -ge $start -and $now -le $end) {
+        Write-Host "Virtual machine $($result.Name) is allowed to be running at $now due to schedule '$($result.Schedule)'."
+        continue
+    }
+
+    Write-Host "Virtual machine $($result.Name) should not be running at $now due to schedule '$($result.Schedule)'."
+
+    $virtualMachineData = [VirtualMachineData]::new()
+    $virtualMachineData.Name = $result.Name
+    $virtualMachineData.Subscription = $result.Subscription
+    $virtualMachineData.ResourceGroup = $result.ResourceGroup
+    $virtualMachineData.Location = $result.Location
+    $virtualMachineData.Schedule = $result.Schedule
+
+    $virtualMachines.Add($virtualMachineData)
+}
+
+return [PSCustomObject]@{
+    VirtualMachines = $virtualMachines
+    Continues       = $null -ne $results.SkipToken
+}
---
+++ src/MaintenanceTasks/Tests/ScanVirtualMachines.Tests.ps1
@@ -0,0 +1,41 @@
+<#
+ Invoke-Pester -Output Detailed .\Tests\ScanVirtualMachines.Tests.ps1
+#>
+
+Describe 'ScanVirtualMachines' -ForEach @(
+    @{
+        Name        = "vm-ok1";
+        Schedule    = '8-16';
+        CurrentTime = '10'
+        Expected    = 0
+    }
+    @{
+        Name        = "vm-ok2";
+        Schedule    = '8-16';
+        CurrentTime = '8'
+        Expected    = 0
+    }
+    @{
+        Name        = "vm-ok3";
+        Schedule    = '8-16';
+        CurrentTime = '16'
+        Expected    = 0
+    }
+    @{
+        Name        = "vm--not-ok1";
+        Schedule    = '8-16';
+        CurrentTime = '7'
+        Expected    = 1
+    }
+) {
+    It 'Validate scanning of <name> with schedule <schedule> at <currentTime> should return <expected>' {
+        Mock -CommandName Search-AzGraph -MockWith { [PSCustomObject]@{ 
+                Name     = $name
+                Schedule = $schedule
+            } }
+        Mock -CommandName Get-Date -MockWith { $currentTime }
+
+        $result = . $PSScriptRoot/../Scripts/ScanVirtualMachines.ps1
+        $result.VirtualMachines.Count | Should -Be $expected
+    }
+}
